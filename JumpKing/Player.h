9d05ec6dfe4693af310f4998a6801fbfd68d3e0f
이재문 <990914s@gmail.com>
2025-07-14 10:40:12 +0900

    리소스 파일 추가

---
+++ JumpKing/Player.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "GameObject.h"
+#include "Animator.h"
+#include "HitBox.h"
+#include "SpriteGo.h"
+
+enum class ChargeType
+{
+    Max,
+	Medium,
+	Low
+};
+
+class Player : public GameObject
+{
+protected:
+    // 공통사용
+    sf::FloatRect windowBound;
+
+    // 히트박스
+    sf::RectangleShape rectLeftArm;
+    sf::RectangleShape rectRightArm;
+    sf::RectangleShape rectLeg;
+    sf::RectangleShape rectHead;
+
+    // 충돌체크 
+    sf::Image maskImage;
+    sf::Vector2u maskSize;
+    sf::Vector2f rectLeftPos;
+    sf::Vector2f rightArmPos;
+    sf::Vector2f leftArmPos;
+    sf::Vector2f headPos;
+  
+    float scaleX;
+    float scaleY;
+
+    // 그리기
+    std::string texId = "graphics/Character_Sprite.png";
+    sf::Sprite character;
+    Animator animator;
+
+    // 이동
+    sf::Vector2f pos;
+    sf::Vector2f direction = { 1.f,0.f };
+    sf::Vector2f jumpDirection = { 0.f,0.f };
+    sf::Vector2f Velocity = { 0.f,0.f };
+
+    bool isJumpChargeActive = false;
+    bool isJumping = false;
+ 
+    float timer = 0.f;
+    float gravity = 0.f;
+
+public:
+    Player(const std::string name = "");
+    ~Player() override = default;
+
+    void SetPosition(const sf::Vector2f& pos) override;
+    void SetRotation(float rot) override;
+    void SetScale(const sf::Vector2f& s) override;
+    void SetOrigin(const sf::Vector2f& o) override;
+    void SetOrigin(Origins preset) override;
+
+    void Init() override;
+    void Release() override;
+    void Reset() override;
+    void Update(float dt) override;
+    void Draw(sf::RenderWindow& window) override;
+
+    void ChargeJump(ChargeType type);
+
+    void HitBox();
+    void CheckCollision_Leg(float dt);
+    void CheckCollision_RightArm();
+    void CheckCollision_LeftArm();
+    void CheckCollision_Head();
+
+};
+
---
+++ JumpKing/SceneGame.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Scene.h"
+#include "SpriteGo.h"
+
+class Player;
+
+class SceneGame : public Scene
+{
+protected:
+	SpriteGo* back1;
+	SpriteGo* back1_Fg;
+	SpriteGo* back1_Hit_Mask;
+
+	Player* character;
+
+	sf::Vector2f windowSize;
+public:
+	SceneGame();
+	~SceneGame() override = default;
+
+	void Init() override;
+	void Enter() override;
+	void Update(float dt) override;
+	void Draw(sf::RenderWindow& window) override;
+	
+};
+
